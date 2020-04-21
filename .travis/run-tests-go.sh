6ecdb2e949521df6738224da0c683a0743d17cd3
srajmane <31947381+srajmane@users.noreply.github.com>
2020-04-21 18:43:15 +0530

    Update run-tests-go.sh

--- .travis/run-tests-go.sh
+++ .travis/run-tests-go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-mvn -q -Dparallel=methods -DthreadCount=1 -Dtest=go.* test 2>&1 | tee $HOME/testlog
+mvn -q -Dparallel=methods -DthreadCount=4 -Dtest=go.* test 2>&1 | tee $HOME/testlog
 cat $HOME/testlog
 ps -aef
 echo $!
